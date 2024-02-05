4dc0eedfb3466d7caba73f0d5071ddca7b5ee67e
Felipe Garcia <felipe-c-garcia@alticelabs.com>
2024-02-05 09:49:44 +0000

    fixes and improves on send request tool

--- ms-tester/src/main/java/com/alticelabs/ccp/exagon/tester/Main.java
+++ ms-tester/src/main/java/com/alticelabs/ccp/exagon/tester/Main.java
@@ -1,6 +1,7 @@
 package com.alticelabs.ccp.exagon.tester;
 
 import com.alticelabs.ccp.exagon.tester.helper.types.BodyGenerator;
+import com.alticelabs.ccp.exagon.tester.runner.SendRequests;
 
 import java.io.IOException;
 import java.net.URI;
@@ -17,30 +18,24 @@ import static java.lang.Thread.sleep;
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException, ExecutionException {
         // Send request
-        List<Future<HttpResponse<String>>> listAnswers = new LinkedList<>();
         Map<Integer, Integer> responseCodes = new HashMap<>();
-        int executionId = new Random().nextInt(999);
-        do{
-            String values = BodyGenerator.PCF_ACCOUNT.generate(executionId + listAnswers.size());
-            //System.out.println(values);
-            HttpClient client = HttpClient.newHttpClient();
-            String port = "8080"; //new String[]{"8080", "8081"}[new Random().nextInt(0,1)];
-            HttpRequest request = HttpRequest.newBuilder()
-                    .header("requestId", UUID.randomUUID().toString())
-                    .header("correlationId", UUID.randomUUID().toString())
-                    .uri(URI.create("http://localhost:"+port+"/account"))
-                    .POST(HttpRequest.BodyPublishers.ofString(values))
-                    .build();
-            long miliPreTime = System.currentTimeMillis();
-            listAnswers.add(client.sendAsync(request, HttpResponse.BodyHandlers.ofString()).thenApply(response -> {
-                System.out.println("Status code: " + response.statusCode()
-                        + " - listAnswer size: " + listAnswers.size()
-                        + " - Time Elapsed: "+ (System.currentTimeMillis() - miliPreTime));
-                return response;
-            }));
-            sleep(100);
-        }while(listAnswers.size() < 1000);
-        sleep(15000);
+        System.out.println(Arrays.toString(buildPorts(args[0])));
+        SendRequests sendRequests = new SendRequests(BodyGenerator.PCF_ACCOUNT,
+                buildPorts(args[0]),
+                "/account",
+                100,
+                10);
+        sendRequests.run();
+        sleep(1000);
+        sendRequests.clearRequests();
+        sendRequests = new SendRequests(BodyGenerator.PCF_ACCOUNT,
+                buildPorts(args[0]),
+                "/account",
+                2000,
+                50);
+        sendRequests.run();
+        sleep(5000);
+        List<Future<HttpResponse<String>>> listAnswers = sendRequests.getListAnswers();
         int timeout = 0;
         List<String[]> dataLines = new ArrayList<>();
         dataLines.add(new String[] {"RequestBody; Response Body; statusCode"});
@@ -49,30 +44,27 @@ public class Main {
             if (future.isDone()) {
                 HttpResponse<String> response = future.get();
                 status = response.statusCode();
-/*                System.out.println("Request: "
-                        + response.request().headers().firstValue("requestId")
-                        + "\nStatus code: " + status);*/
-//                dataLines.add(new String[]{
-//                        response.request().headers().firstValue("requestId") + ";"
-//                        +response.body().subSequence(0,Math.min(50, response.body().length()-1)) + ";"
-//                        +String.valueOf(response.statusCode())
-//                });
-            } else {
+           } else {
+                System.out.println("Indice do problema: " + listAnswers.indexOf(future));
                 future.cancel(true);
-//                System.out.println("Timeout on socket!");
-//                dataLines.add(new String[]{"Timeout por problema de partição;0;0"});
                 timeout++;
             }
             responseCodes.compute(status, (k, v) -> (v == null) ? 1 : v+1);
         }
-
-//        dataLines.stream()
-//                .map(strings -> new CsvUtil().convertToCSV(strings))
-//                .forEach(System.out::println);
-
         System.out.println("Total de pedidos: " + listAnswers.size()
                 + "\nPedidos com erro de particionamento: " + timeout);
-
+        sendRequests.clearRequests();
         responseCodes.forEach((k,v) -> System.out.println("Status code:" + k + " - Quantidade: " + v));
+    }
+
+    private static String[] buildPorts(String arg) {
+        List<String> list = new ArrayList<>();
+        Integer quantity = Integer.valueOf(arg);
+        final Integer port = 8080;
+        for(int a = 0; a < quantity;a++){
+            int curPort = port + a;
+            list.add(Integer.toString(curPort));
+        }
+        return list.toArray(new String[0]);
     }
 }
